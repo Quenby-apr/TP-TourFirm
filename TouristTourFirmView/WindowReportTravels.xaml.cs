20bc1f3edf15167d367b7e3c8ec7aeb9a002ddfa
Долгова А. В <kawa.no.kiri@gmail.com>
2021-05-16 14:30:12 +0400

    role tourist: added sending report to mail function

--- TouristTourFirmView/WindowReportTravels.xaml.cs
+++ TouristTourFirmView/WindowReportTravels.xaml.cs
@@ -4,6 +4,8 @@ using System;
 using System.Windows;
 using TourFirmBusinessLogic.BindingModels;
 using TourFirmBusinessLogic.BusinessLogic;
+using TourFirmBusinessLogic.HelperModels;
+using TourFirmBusinessLogic.Interfaces;
 using Unity;
 
 namespace TouristTourFirmView
@@ -16,13 +18,17 @@ namespace TouristTourFirmView
         [Dependency]
         public IUnityContainer Container { get; set; }
         private readonly TouristReportLogic logic;
+        private readonly ITouristStorage touristStorage;
         private readonly Logger logger;
-        public WindowReportTravels(TouristReportLogic logic)
+
+        public WindowReportTravels(TouristReportLogic logic, ITouristStorage touristStorage)
         {
             InitializeComponent();
             this.logic = logic;
+            this.touristStorage = touristStorage;
             logger = LogManager.GetCurrentClassLogger();
         }
+
         private void ButtonFormReport_Click(object sender, RoutedEventArgs e)
         {
             if (DatePickerFrom.SelectedDate >= DatePickerTo.SelectedDate)
@@ -76,6 +82,44 @@ namespace TouristTourFirmView
                         logger.Warn("Ошибка при попытке формирования отчёта Pdf");
                     }
                 }
+            }
+        }
+
+        private void ButtonSendToMail_Click(object sender, RoutedEventArgs e)
+        {
+            if (DatePickerFrom.SelectedDate >= DatePickerTo.SelectedDate)
+            {
+                MessageBox.Show("Дата начала должна быть меньше даты окончания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                var fileName = "Отчет.pdf";
+
+                logic.SaveTravelsExcursionsGuidesToPdf(new ReportTravelBindingModel
+                {
+                    FileName = fileName,
+                    DateFrom = DatePickerFrom.SelectedDate,
+                    DateTo = DatePickerTo.SelectedDate,
+                    TouristID = App.Tourist.ID
+                });
+
+                MailLogic.MailSend(new MailSendInfo
+                {
+                    MailAddress = App.Tourist.Mail,
+                    Subject = "Отчет по путешествиям",
+                    Text = "Отчет по путешествиям от " + DatePickerFrom.SelectedDate.Value.ToShortDateString() + " по " + DatePickerTo.SelectedDate.Value.ToShortDateString(),
+                    FileName = fileName
+                });
+                MessageBox.Show("Выполнено", "Успех", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Ошибка отправки отчета: " + ex.Message);
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK,
+                MessageBoxImage.Error);
             }
         }
     }
